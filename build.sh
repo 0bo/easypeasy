0e9c7d8cc931626a6b32dc75b43d88a976e8231f
Porog  SSJ <tswoodrow@outlook.com>
2024-10-22 01:07:25 +0000

    build.sh optimize out subshells

--- build.sh
+++ build.sh
@@ -8,25 +8,27 @@ fc-list
 Rscript -e "install.packages('tinytex')"
 Rscript -e "tinytex::install_tinytex(force = TRUE)"
 
-# Build EasyPeasy and translations
+# Build EasyPeasy
 Rscript -e "bookdown::render_book('index.Rmd', 'all')"
 
+# Build EasyPeasy translations
+buildpath=$pwd
 buildtranslation()
 {
+    cd $buildpath
     mkdir -p _book/$1
     cd translations/$1
     Rscript -e "bookdown::render_book('index.Rmd', 'all')"
     cp -r _book/* ../../_book/$1/
 }
-
-( buildtranslation de ) # German
-( buildtranslation it ) # Italian
-( buildtranslation sv ) # Swedish
-( buildtranslation ru ) # Russian
-( buildtranslation so ) # Somali
-( buildtranslation ro ) # Romanian
-( buildtranslation sq ) # Albanian
-( buildtranslation nl ) # Dutch
-( buildtranslation pt-br ) # Brazilian Portuguese
-( buildtranslation pl ) # Polish
-( buildtranslation ko ) # Korean
+buildtranslation de # German
+buildtranslation it # Italian
+buildtranslation sv # Swedish
+buildtranslation ru # Russian
+buildtranslation so # Somali
+buildtranslation ro # Romanian
+buildtranslation sq # Albanian
+buildtranslation nl # Dutch
+buildtranslation pt-br # Brazilian Portuguese
+buildtranslation pl # Polish
+buildtranslation ko # Korean
